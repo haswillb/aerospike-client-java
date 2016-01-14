a510a5dd12bd6d8db3ac49d9761153918b6fbf39
Will Beebe <willb@tune.com>
2016-01-14 15:06:34 -0800

    adding tests

--- tests.sh
+++ tests.sh
@@ -6,8 +6,13 @@ time ./benchmarks/run_benchmarks -h 192.168.99.100 -k 10000 -t 100000
 echo "100% write 10,000 keys 100,000 transactions 100 threads"
 time ./benchmarks/run_benchmarks -h 192.168.99.100 -k 10000 -t 100000 -z 100
 
-#echo "50% read 50% write"
-#time ./benchmarks/run_benchmarks -h 192.168.99.100 -k 100 -w RW,50
+echo "50% read 50% update 10,000 keys 100,000 transactions"
+time ./benchmarks/run_benchmarks -h 192.168.99.100 -k 10000 -t 100000  -w RU,50
 
-#echo "50% read 50% update"
-#time ./benchmarks/run_benchmarks -h 192.168.99.100 -k 100 -w RU,50
+echo "BIG TEST 50% read update 50% 1,000,000 keys 1,000,000 transactions"
+time ./benchmarks/run_benchmarks -h 192.168.99.100 -k 1000000 -t 1000000  -w RU,50
+
+echo "BIGER TEST 50% read update 50% 20,000,000 keys 2,000,000 transactions"
+time ./benchmarks/run_benchmarks -h 192.168.99.100 -k 20000000 -t 2000000  -w RU,50
+
+exit 0
