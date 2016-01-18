bb314d26dd194132e4a7f4b034f82d726791fb97
Will Beebe <willb@tune.com>
2016-01-18 14:20:27 -0800

    making server a var

--- tests.sh
+++ tests.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-echo "100% write 10,000 keys 100,000 transactions"
-time ./benchmarks/run_benchmarks -h 192.168.99.100 -k 10000 -t 100000
+export SERVER_HOST=ec2-54-84-63-185.compute-1.amazonaws.com
 
-echo "100% write 10,000 keys 100,000 transactions 100 threads"
-time ./benchmarks/run_benchmarks -h 192.168.99.100 -k 10000 -t 100000 -z 100
+#echo "100% write 10,000 keys 100,000 transactions"
+#time ./benchmarks/run_benchmarks -h $SERVER_HOST -k 10000 -t 100000
 
-echo "50% read 50% update 10,000 keys 100,000 transactions"
-time ./benchmarks/run_benchmarks -h 192.168.99.100 -k 10000 -t 100000  -w RU,50
+#echo "100% write 10,000 keys 100,000 transactions 100 threads"
+#time ./benchmarks/run_benchmarks -h $SERVER_HOST -k 10000 -t 100000 -z 100
+
+#echo "50% read 50% update 10,000 keys 100,000 transactions"
+#time ./benchmarks/run_benchmarks -h $SERVER_HOST -k 10000 -t 100000  -w RU,50
 
 echo "BIG TEST 50% read update 50% 1,000,000 keys 1,000,000 transactions"
-time ./benchmarks/run_benchmarks -h 192.168.99.100 -k 1000000 -t 1000000  -w RU,50
+time ./benchmarks/run_benchmarks -h $SERVER_HOST -k 1000000 -t 1000000  -w RU,50
 
 echo "BIGER TEST 50% read update 50% 20,000,000 keys 2,000,000 transactions"
-time ./benchmarks/run_benchmarks -h 192.168.99.100 -k 20000000 -t 2000000  -w RU,50
+time ./benchmarks/run_benchmarks -h $SERVER_HOST -k 20000000 -t 2000000  -w RU,50
 
 exit 0
